2cb35cf1b1e8e0d8acba437ae9270e92b1d2ece0
Robin Grether <privat@robingrether.de>
2017-08-04 15:47:56 +0200

    Use snapshot versioning and update to latest iDisguise and Bukkit.

--- idisguise-additions/src/main/java/de/robingrether/idisguise/additions/EventListener.java
+++ idisguise-additions/src/main/java/de/robingrether/idisguise/additions/EventListener.java
@@ -1,5 +1,6 @@
 package de.robingrether.idisguise.additions;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -7,28 +8,31 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
 
+import de.robingrether.idisguise.iDisguise;
+import de.robingrether.idisguise.api.DisguiseAPI;
 import de.robingrether.idisguise.api.DisguiseEvent;
 import de.robingrether.idisguise.api.UndisguiseEvent;
 import de.robingrether.idisguise.disguise.PlayerDisguise;
-import de.robingrether.idisguise.management.DisguiseManager;
 
 public class EventListener implements Listener {
 	
 	private Additions plugin;
+	private DisguiseAPI api;
 	
 	public EventListener(Additions plugin) {
 		this.plugin = plugin;
+		this.api = iDisguise.getInstance().getAPI();
 	}
 	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onEntityTargetLivingEntity(EntityTargetLivingEntityEvent event) {
 		if(event.getTarget() instanceof Player) {
 			Player target = (Player)event.getTarget();
-			if(plugin.getConfiguration().DISABLE_MOB_TARGET && DisguiseManager.getInstance().isDisguised(target)) {
+			if(plugin.getConfiguration().DISABLE_MOB_TARGET && api.isDisguised((OfflinePlayer)target)) {
 				event.setCancelled(true);
 			}
 		}
@@ -41,34 +45,34 @@ public class EventListener implements Listener {
 		Entity damager = event.getDamager();
 		if(damagee instanceof Player && damager instanceof Player) {
 			if(plugin.getConfiguration().UNDISGUISE_PVP) {
-				if(DisguiseManager.getInstance().isDisguisedTo((Player)damagee, (Player)damager)) {
-					DisguiseManager.getInstance().undisguise((Player)damagee);
+				if(api.isDisguisedTo((Player)damagee, (Player)damager)) {
+					api.undisguise((Player)damagee, false);
 					((Player)damagee).sendMessage(plugin.getLanguage().UNDISGUISE_PVP);
 				}
-				if(DisguiseManager.getInstance().isDisguisedTo((Player)damager, (Player)damagee)) {
-					DisguiseManager.getInstance().undisguise((Player)damager);
+				if(api.isDisguisedTo((Player)damager, (Player)damagee)) {
+					api.undisguise((Player)damager, false);
 					((Player)damager).sendMessage(plugin.getLanguage().UNDISGUISE_PVP);
 				}
 			}
 		} else if(damagee instanceof Player) {
-			if(DisguiseManager.getInstance().isDisguised((Player)damagee)) {
+			if(api.isDisguised((OfflinePlayer)damagee)) {
 				if(plugin.getConfiguration().DISABLE_MOB_DAMAGE) {
 					event.setCancelled(true);
 				} else {
 					if(plugin.getConfiguration().UNDISGUISE_PVE) {
-						DisguiseManager.getInstance().undisguise((Player)damagee);
+						api.undisguise((Player)damagee, false);
 						((Player)damagee).sendMessage(plugin.getLanguage().UNDISGUISE_PVE);
 					}
 					if(plugin.getConfiguration().UNDISGUISE_PROJECTILE && damager instanceof Projectile) {
-						DisguiseManager.getInstance().undisguise((Player)damagee);
+						api.undisguise((Player)damagee, false);
 						((Player)damagee).sendMessage(plugin.getLanguage().UNDISGUISE_PROJECTILE);
 					}
 				}
 			}
 		} else if(damager instanceof Player) {
 			if(plugin.getConfiguration().UNDISGUISE_PVE) {
-				if(DisguiseManager.getInstance().isDisguised((Player)damager)) {
-					DisguiseManager.getInstance().undisguise((Player)damager);
+				if(api.isDisguised((OfflinePlayer)damager)) {
+					api.undisguise((Player)damager, false);
 					((Player)damager).sendMessage(plugin.getLanguage().UNDISGUISE_PVE);
 				}
 			}
@@ -76,8 +80,9 @@ public class EventListener implements Listener {
 	}
 	
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
-		if(plugin.getConfiguration().DISABLE_ITEM_PICK_UP && DisguiseManager.getInstance().isDisguised(event.getPlayer())) {
+	public void onEntityPickupItem(EntityPickupItemEvent event) {
+		if(!(event.getEntity() instanceof Player)) return;
+		if(plugin.getConfiguration().DISABLE_ITEM_PICK_UP && api.isDisguised((OfflinePlayer)event.getEntity())) {
 			event.setCancelled(true);
 		}
 	}
@@ -98,7 +103,7 @@ public class EventListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		if(plugin.getConfiguration().MODIFY_DISPLAY_NAME && DisguiseManager.getInstance().getDisguise(event.getPlayer()) instanceof PlayerDisguise) {
+		if(plugin.getConfiguration().MODIFY_DISPLAY_NAME && api.getDisguise((OfflinePlayer)event.getPlayer()) instanceof PlayerDisguise) {
 			event.getPlayer().setDisplayName(event.getPlayer().getName());
 		}
 	}
